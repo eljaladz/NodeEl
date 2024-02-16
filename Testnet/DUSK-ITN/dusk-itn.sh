2d713bba6bc04b7cb01259aaf7268e5ed521c180
El Jalad <95561074+eljaladz@users.noreply.github.com>
2024-02-17 02:05:06 +0700

    Update dusk-itn.sh

--- Testnet/DUSK-ITN/dusk-itn.sh
+++ Testnet/DUSK-ITN/dusk-itn.sh
@@ -2,18 +2,45 @@
 
 #Instalasi Rusk
 echo "Installing Rusk.."
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
 curl --proto '=https' --tlsv1.2 -sSfL https://github.com/dusk-network/itn-installer/releases/download/v0.1.0/itn-installer.sh | sudo sh
 
 echo "Installation Complete!"
 
-sleep 3 #Jeda waktu
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
 
 #Konfigurasi Rusk
 echo "Providing CONSENSUS KEY file.."
 
-sleep 3 #Jeda waktu
+sleep 1
 
 echo "Restoring your Dusk Wallet.."
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
 rusk-wallet restore
 
 echo "Wallet Restored!"
@@ -21,45 +48,103 @@ echo "Wallet Restored!"
 sleep 3 #Jeda waktu
 
 echo "Exporting CONSENSUS KEY.."
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
 rusk-wallet export -d /opt/dusk/conf -n consensus.keys
 
 echo "CONSENSYS KEY exported!"
 
-sleep 3 #Jeda waktu
+sleep 3
 
 echo "Set your DUSK CONSENSUS KEY PASS.."
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
 sh /opt/dusk/bin/setup_consensus_pwd.sh
 
 echo "Your DUSK CONSENSUS KEY PASS has been set"
 
-sleep 3 #Jeda waktu
+sleep 3
 
 echo "Preparing to launch your node.."
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
 service rusk start
 
 sleep 3 #Jeda waktu
 
-echo "Syncing yopur node.."
+echo "Syncing your node.."
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
 grep "block accepted" /var/log/rusk.log
 
 sleep 15 #Jeda waktu sinkronisasi
 
 echo "Completed!"
 
-sleep 5 #Jeda waktu ke step selanjutnya
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
 
 #Staking tDUSK
 echo "Your wallet has 1100 tDUSK, enter the amount of tDUSK to stake at least 1000 tDUSK and save some of tDUSK for the gas fee in your wallet."
-read -p "Enter the amount of tDUSK you want to stake = " stkAmt
+sleep 3
+read -p "Enter the amount of tDUSK you want to stake = " stakeAmount
 
-rusk-wallet stake --amt "$stakeAmt"
+rusk-wallet stake --amt "$stakeAmount"
 
-echo "'$stkAmt' tDUSK staked!"
+echo "$stakeAmount tDUSK staked!"
 
 sleep 3 #Jeda waktu
 
 #Mendapatkan informasi staking
 echo "Getting your staking information.."
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
 rusk-wallet stake-info
 
 echo "Done!"
