58181c6d61a813ec547d9d71f6c2a4722da367cc
El Jalad <95561074+eljaladz@users.noreply.github.com>
2024-03-25 19:33:24 +0700

    Update dusk-itn.sh
    Installer update from v0.1.9 to v0.2.0

--- Testnet/DUSK-ITN/dusk-itn.sh
+++ Testnet/DUSK-ITN/dusk-itn.sh
@@ -12,7 +12,7 @@ sleep 1
 echo "."
 sleep 1
 
-curl --proto '=https' --tlsv1.2 -sSfL https://github.com/dusk-network/itn-installer/releases/download/v0.1.9/itn-installer.sh | sudo sh
+curl --proto '=https' --tlsv1.2 -sSfL https://github.com/dusk-network/node-installer/releases/download/v0.2.0/node-installer.sh | sudo sh
 
 echo "Installation Complete!"
 
