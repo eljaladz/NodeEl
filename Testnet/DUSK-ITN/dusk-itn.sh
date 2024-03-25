bd090e2ca66c7d3e32e766a7094170070060f111
El Jalad <95561074+eljaladz@users.noreply.github.com>
2024-03-25 20:48:43 +0700

    Update dusk-itn.sh
    Add some update and improvements

--- Testnet/DUSK-ITN/dusk-itn.sh
+++ Testnet/DUSK-ITN/dusk-itn.sh
@@ -45,7 +45,14 @@ rusk-wallet restore
 
 echo "Wallet Restored!"
 
-sleep 3 #Jeda waktu
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
 
 echo "Exporting CONSENSUS KEY.."
 
@@ -62,9 +69,16 @@ rusk-wallet export -d /opt/dusk/conf -n consensus.keys
 
 echo "CONSENSYS KEY exported!"
 
-sleep 3
+echo "."
+sleep 1
 
-echo "Set your DUSK CONSENSUS KEY PASS.."
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "Setting up your DUSK CONSENSUS KEY PASS.."
 
 echo "."
 sleep 1
@@ -79,7 +93,14 @@ sh /opt/dusk/bin/setup_consensus_pwd.sh
 
 echo "Your DUSK CONSENSUS KEY PASS has been set"
 
-sleep 3
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
 
 echo "Preparing to launch your node.."
 
@@ -94,47 +115,11 @@ sleep 1
 
 service rusk start
 
-sleep 3 #Jeda waktu
-
 echo "Syncing your node.."
 
-echo "."
-sleep 1
-
-echo "."
-sleep 1
-
-echo "."
-sleep 1
-
-grep "block accepted" /var/log/rusk.log
-
-sleep 15 #Jeda waktu sinkronisasi
-
-echo "Completed!"
-
-echo "."
-sleep 1
-
-echo "."
-sleep 1
-
-echo "."
-sleep 1
-
-#Staking tDUSK
-echo "Your wallet has 1100 tDUSK, enter the amount of tDUSK to stake at least 1000 tDUSK and save some of tDUSK for the gas fee in your wallet."
 sleep 3
-read -p "Enter the amount of tDUSK you want to stake = " stakeAmount
 
-rusk-wallet stake --amt "$stakeAmount"
-
-echo "$stakeAmount tDUSK staked!"
-
-sleep 3 #Jeda waktu
-
-#Mendapatkan informasi staking
-echo "Getting your staking information.."
+echo "Wait till your node Synced, then stake your nDUSK"
 
 echo "."
 sleep 1
@@ -144,18 +129,18 @@ sleep 1
 
 echo "."
 sleep 1
-
-rusk-wallet stake-info
-
-echo "Done!"
-
-sleep 3 #Jeda waktu
-
-echo "To see if your node is participating in consensus and creating blocks, use command below:"
-echo " tail -F /var/log/rusk.log | grep "execute_state_transition""
-echo "FYI, that this can take a while, given that your stake needs at least 2 epochs, or 4320 blocks, to mature. Your stake, relative to the total stake, also plays a factor. "
-
-sleep 1 #Jeda waktu
 
 echo "Join us at https://t.me/nscryptoid"
+sleep 1
 echo "Follow me on X https://x.com/ejldzz"
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+echo "."
+sleep 1
+
+curl --silent https://raw.githubusercontent.com/Znomon/Dusk-ITN2-Node-Tool/main/node_height_checker_w_estimate.py | python3
